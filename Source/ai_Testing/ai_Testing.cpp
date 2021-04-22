a886bb20860959a5ec63180d178cfc8e7edfd154
CM\Aboud.Malki <aboud-malki@hotmail.com>
2021-04-22 11:37:15 +0200

    Collab commit

---
+++ Source/ai_Testing/ai_Testing.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ai_Testing.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ai_Testing, "ai_Testing" );
